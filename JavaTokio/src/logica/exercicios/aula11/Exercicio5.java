f7d021bfd9c71951d8a35ebed08758fe7016b61d
gcsants <guisantoscosta9@gmail.com>
2024-09-15 22:23:45 -0300

    exercicios

--- JavaTokio/src/logica/exercicios/aula11/Exercicio5.java
+++ JavaTokio/src/logica/exercicios/aula11/Exercicio5.java
@@ -4,8 +4,31 @@ public class Exercicio5 {
 
 	public static void main(String[] args) {
 		
+		/*
+		Crie uma classe com um nome qualquer com o método main() padrão.
+		Crie uma classe com o nome “OperacoesMatematicas” 
+		sem método main por padrão.
+		Crie essas duas classes no mesmo pacote.
+		Na classe OperacoesMatematicas, crie um método que receba 
+		como parâmetro a operação (+, -, *, /) e dois números. 
 		
+		Esse método deve chamar a função correspondente a sua operação 
+		e retornar o resultado.
+		
+		Além disso, na classe OperacoesMatematicas, você deve criar os 
+		métodos para cada operação. 
+		Esses métodos não podem ser acessados fora da classe 
+		OperacoesMatematicas
+		
+		Na classe com o método main(), chame o método da classe 
+		OperacoesMatematicas passando como parâmetro a operação 
+		desejada e dois números. Exiba o resultado.
+		*/
+		
+		double resultado = OperacoesMatematicas.operacao('-', 4, 3);
+		System.out.println(resultado);
 
+		
 	}
 
 }
--- JavaTokio/src/logica/exercicios/aula11/OperacoesMatematicas.java
+++ JavaTokio/src/logica/exercicios/aula11/OperacoesMatematicas.java
@@ -1,10 +1,24 @@
 package logica.exercicios.aula11;
 
 public class OperacoesMatematicas {
-
-	public static void main(String[] args) {
-
+	
+	public static double operacao (char op, double num1, double num2) {
+		switch (op) {
+			case '+':
+				return somar(num1, num2);
+			case '-':
+				return num1 - num2;
+			case '*':
+				return num1 * num2;
+			case '/':
+				return num1 / num2;
+		}
 		
+		return -1;
 	}
-
+	
+	private static double somar(double a, double b) {
+		return a + b;
+	}
+	
 }
